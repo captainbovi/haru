6d5cdeae74640a8d91fb062fbc9c0dd117aad6bd
unknown <1262813433@qq.com>
2022-09-28 15:27:30 +0800

    website first version

--- wp-login.php
+++ wp-login.php
@@ -108,7 +108,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 </script>
 <script src="/wp-includes/js/wp-util.min.js?ver=6.0.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"0f933f5afa"};
+var userProfileL10n = {"user_id":"0","nonce":"da68901c71"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
